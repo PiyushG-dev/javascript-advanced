9f2edd2e06fcaea905b23a54caac174c43b4909a
Piyush Goswami <pjgpiyush@gmail.com>
2024-03-03 16:14:09 +0530

    test

---
+++ 03_advance/test.js
@@ -0,0 +1,24 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+const month = ['jan', 'feb', 'march', 'april', 'may', 'june', 'july', 'aug', 'sept', 'oct', 'nov', 'dec']
+
+const value = 15
+
+switch (value) {
+    case 2:
+        console.log('its two')
+        break;
+    case 15:
+        console.log('its fif')
+        break;
+
+    default:
+        console.log('default')
+        break;
+}
+
+
+if("0"){
+    console.log('0 is true')
+}else{
+    console.log('0 equates to false')
+}
